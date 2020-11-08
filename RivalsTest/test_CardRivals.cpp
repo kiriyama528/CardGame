baa2e728f46ad30c932bc7dd416d72ce63ecace6
桐山 智也 <trr.trr.528@gmail.com>
2020-11-08 19:29:33 +0900

    Rivalsクラスのコンストラクタとload関数のテストコードを記述した。テストコードプロジェクトにopencvへのリンクを追加し、テストコードのビルド、実行ができるようにした。テストオールグリーン

--- RivalsTest/test_CardRivals.cpp
+++ RivalsTest/test_CardRivals.cpp
@@ -1,5 +1,8 @@
+// Cardライブラリが読み込めなくてエラーが出ているみたい
+// CardGameTestプロジェクトと比較してリンカー関連を修正しよう！
+
 #include "pch.h"
-#include "CardListRivals.h"
+#include "CardRivals.h"
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
@@ -15,12 +18,23 @@ protected:
 	virtual void TearDown() {
 		// do nothing
 	}
+
 };
 
-TEST_F(UnitTestCardRivals, load) {
-	// making
+TEST_F(UnitTestCardRivals, CardRivals) {
+	CardRivals rivals;
+	int exp_power = 0;
+	int act_power = rivals.get_power();
+	EXPECT_EQ(exp_power, act_power);
 }
 
-TEST_F(UnitTestCardRivals, get_power) {
-	// making
+TEST_F(UnitTestCardRivals, load) {
+	CardRivals rivals;
+	char str[128];
+	int exp_power = 7;
+	sprintf(str, "sample_name,sample_ability,sample_img,%d", exp_power);
+	rivals.load(str);
+	int act_power = rivals.get_power();
+	EXPECT_EQ(exp_power, act_power);
 }
+
