d4170030c0377ff427a4a83a2cc07b8b17f3020b
桐山 智也 <trr.trr.528@gmail.com>
2021-02-11 21:14:08 +0900

    FieldRivals.cppのテストファイルのみ作成。中身は今後作る

---
+++ RivalsTest/test_FieldRivals.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "FieldRivals.h"
+
+class UnitTestFieldRivals : public ::testing::Test {
+protected:
+	virtual void SetUp() {
+		// do nothing
+	}
+
+	virtual void TearDown() {
+		// do nothing
+	}
+
+};
