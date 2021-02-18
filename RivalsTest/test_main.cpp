a0c2d49f7a45fda1fe70943ac23d813e9632dcaf
桐山 智也 <trr.trr.528@gmail.com>
2021-02-18 19:35:12 +0900

    テストでエラーが出てる。テストの予想値が異なっているので、予想値の値渡しあたりにバグがあるかもしれない。そうでなければ、姫と王子の戦闘にバグがある

--- RivalsTest/test_main.cpp
+++ RivalsTest/test_main.cpp
@@ -34,7 +34,7 @@ protected:
 		const unsigned int *exp_p2)
 	{
 		for (int i = 0; i < p1_idx.size(); i++) {
-			printf("\ridx:%d", i);
+			printf("\ridx:%d, p1_idx:%d, p2_idx:%d\n", i, p1_idx[i], p2_idx[i]);
 			// 今勝負で使用するカード
 			CardRivals *card_p1 = dynamic_cast<CardRivals*>(cards_[p1_idx[i]]);
 			CardRivals *card_p2 = dynamic_cast<CardRivals*>(cards_[p2_idx[i]]);
@@ -54,8 +54,8 @@ protected:
 
 // TODO : 適切な名前にする
 TEST_F(TestRivalsMain, case1) {
-	const vector<int> p1_idx = { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3 };
-	const vector<int> p2_idx = { 0, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7, 2, 3, 4, 5, 6, 7, 3, 4, 5, 6, 7 };
+	const vector<int> p1_idx    = { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3 };
+	const vector<int> p2_idx    = { 0, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7, 2, 3, 4, 5, 6, 7, 3, 4, 5, 6, 7 };
 	const unsigned int exp_p1[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 4, 0, 2, 0, 0, 0, 0, 0, 2, 0, 1, 0 };
 	const unsigned int exp_p2[] = { 0, 0, 0, 0, 0, 7, 0, 0, 0, 4, 0, 2, 1, 1, 0, 0, 0, 2, 1, 1, 1, 0, 0, 1, 0, 1 };
 
@@ -65,8 +65,8 @@ TEST_F(TestRivalsMain, case1) {
 
 // TODO : 適切な名前にする
 TEST_F(TestRivalsMain, case2) {
-	const vector<int> p1_idx = { 4, 4, 4, 4, 5, 5, 5, 6, 6, 7, 7 };
-	const vector<int> p2_idx = { 4, 5, 6, 7, 5, 6, 7, 6, 7, 7, 1 };
+	const vector<int> p1_idx    = { 4, 4, 4, 4, 5, 5, 5, 6, 6, 7, 7 };
+	const vector<int> p2_idx    = { 4, 5, 6, 7, 5, 6, 7, 6, 7, 7, 1 };
 	const unsigned int exp_p1[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 };
 	const unsigned int exp_p2[] = { 0, 3, 1, 1, 0, 2, 1, 0, 2, 0, 4 };
 
