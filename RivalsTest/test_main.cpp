cce4eabb7390d4d906dbe954b171826b2fda9d98
桐山 智也 <trr.trr.528@gmail.com>
2021-02-20 00:56:40 +0900

    やはり姫と王子の対戦結果が間違っているようだ。Fieldクラスに対してテストが必要

--- RivalsTest/test_main.cpp
+++ RivalsTest/test_main.cpp
@@ -54,10 +54,10 @@ protected:
 
 // TODO : 適切な名前にする
 TEST_F(TestRivalsMain, case1) {
-	const vector<int> p1_idx    = { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3 };
-	const vector<int> p2_idx    = { 0, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7, 2, 3, 4, 5, 6, 7, 3, 4, 5, 6, 7 };
-	const unsigned int exp_p1[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 4, 0, 2, 0, 0, 0, 0, 0, 2, 0, 1, 0 };
-	const unsigned int exp_p2[] = { 0, 0, 0, 0, 0, 7, 0, 0, 0, 4, 0, 2, 1, 1, 0, 0, 0, 2, 1, 1, 1, 0, 0, 1, 0, 1 };
+	const vector<int> p1_idx    = { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1};
+	const vector<int> p2_idx    = { 0, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7};
+	const unsigned int exp_p1[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 4};
+	const unsigned int exp_p2[] = { 0, 0, 0, 0, 0, 7, 0, 0, 0, 4, 0, 2, 1, 1, 0};
 
 	PlayAndIsExpected(p1_idx, p2_idx, exp_p1, exp_p2);
 }
@@ -65,6 +65,17 @@ TEST_F(TestRivalsMain, case1) {
 
 // TODO : 適切な名前にする
 TEST_F(TestRivalsMain, case2) {
+	const vector<int> p1_idx    = { 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3 };
+	const vector<int> p2_idx    = { 2, 3, 4, 5, 6, 7, 3, 4, 5, 6, 7 };
+	const unsigned int exp_p1[] = { 0, 2, 0, 0, 0, 0, 0, 2, 0, 1, 0 };
+	const unsigned int exp_p2[] = { 0, 0, 2, 1, 1, 1, 0, 0, 1, 0, 1 };
+
+	PlayAndIsExpected(p1_idx, p2_idx, exp_p1, exp_p2);
+}
+
+
+// TODO : 適切な名前にする
+TEST_F(TestRivalsMain, case3) {
 	const vector<int> p1_idx    = { 4, 4, 4, 4, 5, 5, 5, 6, 6, 7, 7 };
 	const vector<int> p2_idx    = { 4, 5, 6, 7, 5, 6, 7, 6, 7, 7, 1 };
 	const unsigned int exp_p1[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 };
@@ -75,7 +86,7 @@ TEST_F(TestRivalsMain, case2) {
 
 
 // TODO : 適切な名前にする
-TEST_F(TestRivalsMain, case3) {
+TEST_F(TestRivalsMain, careful1) {
 	// 要注意確認
 	const vector<int> p1_idx = { 5, 5, 5, 5, 5, 5, 5, 5, 6, 5, 4 };
 	const vector<int> p2_idx = { 0, 1, 2, 3, 4, 5, 6, 4, 7, 7, 2 };
@@ -89,7 +100,7 @@ TEST_F(TestRivalsMain, case3) {
 
 
 // TODO : 適切な名前にする
-TEST_F(TestRivalsMain, case4) {
+TEST_F(TestRivalsMain, careful2) {
 	// 要注意確認
 	const vector<int> p1_idx = { 6, 1, 6, 1, 6, 2, 6, 3, 6, 4 };
 	const vector<int> p2_idx = { 7, 2, 7, 3, 7, 3, 7, 3, 7, 5 };
