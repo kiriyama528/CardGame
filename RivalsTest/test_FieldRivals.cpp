f6923f45020050b3cfff8524bbec32a0b4a4b4e1
桐山 智也 <trr.trr.528@gmail.com>
2021-02-20 23:03:26 +0900

    DummyCardRivalsクラスの作成。FieldRivalsクラスのテストを書く準備だけした。今後は各種能力からテストを記述していく

--- RivalsTest/test_FieldRivals.cpp
+++ RivalsTest/test_FieldRivals.cpp
@@ -1,31 +1,42 @@
 #include "pch.h"
+#include "CardListRivals.h"
 #include "FieldRivals.h"
+
+class DummyCardRivals : public CardRivals {
+	/* メモ
+	string name;         // カードの名前
+	string ability_text; // カード効果のテキスト文
+	cv::Mat img;         // カードの画像
+	unsigned int power;
+	*/
+public:
+	DummyCardRivals(
+		const char* _name,
+		const char* _ability_text,
+		unsigned int _power)
+	{
+		name = _name;
+		ability_text = _ability_text;
+		power = _power;
+	}
+
+	virtual ~DummyCardRivals() {
+		// do nothing
+	}
+};
 
 class UnitTestFieldRivals : public ::testing::Test {
 protected:
+	FieldRivals *field_;
 	virtual void SetUp() {
-		// do nothing
+		field_ = new FieldRivals;
 	}
 
 	virtual void TearDown() {
-		// do nothing
+		delete field_;
 	}
 
 };
-
-
-TEST_F(UnitTestFieldRivals, playCards)
-{
-	// making
-	EXPECT_TRUE(false);
-}
-
-
-TEST_F(UnitTestFieldRivals, isReveal)
-{
-	// making
-	EXPECT_TRUE(false);
-}
 
 
 TEST_F(UnitTestFieldRivals, branchAbility)
@@ -36,6 +47,13 @@ TEST_F(UnitTestFieldRivals, branchAbility)
 
 
 TEST_F(UnitTestFieldRivals, abilityDraw)
+{
+	// making
+	EXPECT_TRUE(false);
+}
+
+
+TEST_F(UnitTestFieldRivals, isReveal)
 {
 	// making
 	EXPECT_TRUE(false);
@@ -93,6 +111,19 @@ TEST_F(UnitTestFieldRivals, enchantment)
 
 TEST_F(UnitTestFieldRivals, ability)
 {
+	// making
+	EXPECT_TRUE(false);
+}
+
+
+TEST_F(UnitTestFieldRivals, playCards)
+{
+	DummyCardRivals p1_card("p1_card", "p1_ability", 4);
+	DummyCardRivals p2_card("p1_card", "p1_ability", 6);
+	unsigned int p1_wins = 0;
+	unsigned int p2_wins = 0;
+	field_->playCards(&p1_card, &p2_card, p1_wins, p2_wins);
+
 	// making
 	EXPECT_TRUE(false);
 }
